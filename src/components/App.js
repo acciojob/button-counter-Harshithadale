c3d87efd50b9a63d0f8d73575bff8df4b429d19b
harshithadale <daleharshi6045@gmail.com>
2025-04-10 09:39:20 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [num,setNum] = React.useState(0)
+  const handleInput=()=>{
+    setNum(num + 1)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {num} times</p>
+        <button onClick={handleInput}>Click me</button>
     </div>
   )
 }
